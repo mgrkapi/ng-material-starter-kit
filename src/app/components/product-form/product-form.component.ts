534570dd3a2a2b75b11a75a2881f573acc2dc9cb
mgrkapi <mgrkapi@wp.pl>
2022-11-29 15:45:53 +0100

    feat:product form with categories implemented

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { ProductModel } from '../../models/product.model';
 import { ProductService } from '../../services/product.service';
+import { ProductsExtendedService } from '../../services/products-extended.service';
 
 @Component({
   selector: 'app-product-form',
@@ -17,17 +20,18 @@ export class ProductFormComponent {
     image: new FormControl(),
     category: new FormControl(),
   });
+  readonly list$: Observable<ProductModel[]> = this._productsExtendedService.getAll();
 
-  constructor(private _productService: ProductService) {
+  constructor(private _productService: ProductService, private _productsExtendedService: ProductsExtendedService) {
   }
 
   onProductFormSubmitted(productForm: FormGroup): void {
     this._productService.create({
-      title:productForm.get('title')?.value,
-      category:productForm.get('category')?.value,
-      price:productForm.get('price')?.value,
-      description:productForm.get('description')?.value,
-      image:productForm.get('image')?.value,
+      title: productForm.get('title')?.value,
+      category: productForm.get('category')?.value,
+      price: productForm.get('price')?.value,
+      description: productForm.get('description')?.value,
+      image: productForm.get('image')?.value,
     }).subscribe();
   }
 }
---
+++ src/app/services/products-extended.service-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ProductsExtendedService } from './products-extended.service';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [ProductsExtendedService],
+  exports: []
+})
+export class ProductsExtendedServiceModule {
+}
